a00de57d3a46b50be8cf12b38c2f3ad6e05df13f
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-15 23:28:31 +0000

    finished square calc part

--- app.rb
+++ app.rb
@@ -14,5 +14,13 @@ erb(:square)
 end
 
 get("/square/results") do
+  @the_num = params.fetch("number").to_f
 
+  @the_square = @the_num ** 2
 erb(:square_results)
+end
+
+get("/square_root/new") do
+
+  erb(:square_root)
+end
